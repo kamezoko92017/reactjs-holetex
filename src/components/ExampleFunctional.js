0daaae41d8e8de608205c34ba5e16cf960e2587c
albertbrasi <albertbrasi001@gmail.com>
2022-07-08 11:11:06 +0700

    useMemo: thêm điều kiện để chạy lại hàm

--- src/components/ExampleFunctional.js
+++ src/components/ExampleFunctional.js
@@ -12,15 +12,19 @@ function expensiveFunction(number) {
 
 export default function ExampleFunctional() {
     const [count, setCount] = useState(0);
+    const [num, setNum] = useState(20)
 
+    //expensiveFunction sử dụng biến num nên cần chạy lại hàm expensiveFunction
+    //mỗi khi state num thay đổi, do vậy phải thêm [num] vào tham số thứ 2
     const number = useMemo(() => {
-        return expensiveFunction(10)
-    }, [])
+        return expensiveFunction(num)
+    }, [num])
 
     return (
         <div>
             <p>Count: {count} times</p>
             <button onClick={() => setCount(count + 1)}>Add</button>
+            <button onClick={() => setNum(num + 1)}>Num</button>
 
             <p>Number: {number}</p>
         </div>
